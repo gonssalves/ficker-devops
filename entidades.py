4b855bf90d51ada7fd79ce672a5932832761bb57
Vinícius Gonçalves <vgo1@aluno.ifal.edu.br>
2023-04-29 23:34:29 -0300

    gitignore

--- entidades.py
+++ entidades.py
@@ -1,75 +1,89 @@
 from app import db
 
+#criação das tabelas
 class Usuario(db.Model):
     __tablename__ = 'usuarios'
-    cod_usuario = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     nom_real = db.Column(db.String(64), nullable=False)
     nom_usuario = db.Column(db.String(64), unique=True,)
     sen_usuario = db.Column(db.String(), nullable=False)
     eml_usuario = db.Column(db.String(), unique=True, nullable=False)
+    
+    #cria uma nova propriedade que vai apontar para classes que recebem a pk do Usuario e carregar vários instâncias
+    orcamentos_u = db.relationship('Orcamento', backref='usuario', lazy=True)
+    transacoes_entrada_u = db.relationship('TransacaoEntrada', backref='usuario', lazy=True)
+    transacoes_saida_u = db.relationship('TransacaoSaida', backref='usuario', lazy=True)
+    categorias_u = db.relationship('Categoria', backref='usuario', lazy=True)
+    objetivos_u = db.relationship('Objetivo', backref='usuario', lazy=True)
+    transacoes_cofrinho_u = db.relationship('TransacaoCofrinho', backref='usuario', lazy=True)
 
     def __repr__(self):
-        return f'<Usuario {self.nom_usuario} | {self.nom_real}>'
+        return f'<Usuário: {self.nom_usuario} | Nome: {self.nom_real}>'
     
 class Orcamento(db.Model):
     __tablename__ = 'orcamentos'
-    cod_orcamento = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     val_orcamento = db.Column(db.Float(64))
     dat_orcamento = db.Column(db.Date(), nullable=False)
-
+    cod_usuario = db.Column(db.Integer, db.ForeignKey('usuarios.id'))
+    
     def __repr__(self):
-        return f'<Orcamento {self.val_orcamento} | {self.dat_orcamento}>'
+        return f'<Valor Orçamento: {self.val_orcamento} | Data: {self.dat_orcamento}>'
     
 class TransacaoEntrada(db.Model):
     __tablename__ = 'transacoes_entrada'
-    cod_entrada = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     dsc_entrada = db.Column(db.String(64), nullable=False)
     dat_entrada = db.Column(db.Date(), nullable=False)
     val_entrada = db.Column(db.Float(64))
+    cod_usuario = db.Column(db.Integer, db.ForeignKey('usuarios.id'))
 
     def __repr__(self):
-        return f'<TransacaoEntrada {self.dsc_entrada} | {self.dsc_entrada} | {self.dsc_entrada}>'
+        return f'<Transação Entrada: {self.dsc_entrada} | Data: {self.dat_entrada} | Valor: {self.val_entrada}>'
     
 class TransacaoSaida(db.Model):
     __tablename__ = 'transacoes_saida'
-    cod_saida = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     dsc_saida = db.Column(db.String(64), nullable=False)
     dat_saida = db.Column(db.Date(), nullable=False)
     val_saida = db.Column(db.Float(64))
+    cod_usuario = db.Column(db.Integer, db.ForeignKey('usuarios.id'))
+    cod_categoria = db.Column(db.Integer, db.ForeignKey('categorias.id'))
 
     def __repr__(self):
-        return f'<TransacaoEntrada {self.dsc_saida} | {self.dat_saida} | {self.val_saida}>'
+        return f'<Transação Saída: {self.dsc_saida} | Data: {self.dat_saida} | Valor: {self.val_saida}>'
 
 class Categoria(db.Model):
     __tablename__ = 'categorias'
-    cod_categoria = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     dsc_categoria = db.Column(db.String(64), nullable=False)
-    
+    cod_usuario = db.Column(db.Integer, db.ForeignKey('usuarios.id'))
+
+    transacoes_saida_c = db.relationship('TransacaoSaida', backref='categoria', lazy=True)
+
     def __repr__(self):
-        return f'<Categoria {self.dsc_categoria}>'
+        return f'<Categoria: {self.dsc_categoria}>'
     
 class Objetivo(db.Model):
     __tablename__ = 'objetivos'
-    cod_objetivo = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     dsc_objetivo = db.Column(db.String(64), nullable=False)
-    cor_objetivo = db.Column(db.String(64), nullable=True,)
+    cor_objetivo = db.Column(db.String(64), nullable=True)
+    cod_usuario = db.Column(db.Integer, db.ForeignKey('usuarios.id'))
    
+    transacoes_cofrinho_o = db.relationship('TransacaoCofrinho', backref='objetivo', lazy=True)
+
     def __repr__(self):
-        return f'<Objetivo {self.dsc_objetivo} | {self.cor_objetivo}>'
+        return f'<Objetivo: {self.dsc_objetivo} | Cor: {self.cor_objetivo}>'
     
 class TransacaoCofrinho(db.Model):
     __tablename__ = 'transacoes_cofrinho'
-    cod_cofrinho = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    tip_transacao = db.Column(db.String(64), nullable=False)
     dat_transacao = db.Column(db.Date(), nullable=False)
     val_cofrinho = db.Column(db.Float(64), nullable=False)
-
+    cod_usuario = db.Column(db.Integer, db.ForeignKey('usuarios.id'))
+    cod_objetivo = db.Column(db.Integer, db.ForeignKey('objetivos.id'))
+   
     def __repr__(self):
-        return f'<TransacaoCofrinho {self.dat_transacao}> | {self.val_cofrinho}'
-    
-class Acao(db.Model):
-    __tablename__ = 'acoes'
-    cod_acao = db.Column(db.Integer, primary_key=True)
-    dsc_acao = db.Column(db.String(64), nullable=False)
-
-    def __repr__(self):
-        return f'<Acao {self.dsc_acao}>'
+        return f'<Transação Cofrinho: {self.tip_transacao} | Data: {self.dat_transacao}> | Valor: {self.val_cofrinho}'
