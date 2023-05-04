56255887595900abab8aa5fbf162cb349316ecb1
Vinícius Gonçalves <vgo1@aluno.ifal.edu.br>
2023-05-03 23:58:40 -0300

    feat: adiciona views de autorização

---
+++ views/auth.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, redirect, url_for
+from flask_login import login_required, logout_user
+
+auth = Blueprint('auth', __name__)
+
+#views responsáveis pela autorização
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    ...
+
+@auth.route('/signup', methods=['GET', 'POST'])
+def signup():
+    ...
+
+@auth.route('/forgot-password', methods=['GET', 'POST'])
+def forgot_password():
+    ...
+
+@auth.route('/logout', methods=['GET'])
+@login_required
+def logout():
+    logout_user()
+    return redirect((url_for('main.index')))
