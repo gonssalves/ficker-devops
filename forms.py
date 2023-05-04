124a038bb2eda5f831d0a69de539e3f4a9cbb848
Vinícius Gonçalves <vgo1@aluno.ifal.edu.br>
2023-05-04 11:56:46 -0300

    feat: adiciona remember_me

--- forms.py
+++ forms.py
@@ -1,14 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import InputRequired, Length
 
 class LoginForm(FlaskForm):
     email = StringField(validatos=[InputRequired()])
     password = PasswordField(validators=[InputRequired()])
+    remember_me = BooleanField('Me mantenha conectado')
     submit = SubmitField('Entrar')
 
 class SignupForm(FlaskForm):
-    name = StringField(validatos=[InputRequired()])
+    real_name = StringField(validatos=[InputRequired()])
+    username = StringField(validatos=[InputRequired()])
     email = StringField(validators=[InputRequired()])
     password = PasswordField(validators=[InputRequired()])
     password2 = PasswordField(validators=[InputRequired()])
