1bda70358ce6671619dc0631b57143d61ca2bec6
Vinícius Gonçalves <vgo1@aluno.ifal.edu.br>
2023-05-04 00:32:48 -0300

    feat: insere login_required nas views

--- views/main.py
+++ views/main.py
@@ -1,4 +1,6 @@
 from flask import Blueprint
+from flask_login import login_required
+
 
 main = Blueprint('main', __name__)
 
@@ -7,29 +9,36 @@ def index():
     ...
 
 @main.route('/home', methods=['GET', 'POST', 'PUT'])
+@login_required
 def home():
     ...
 
 @main.route('/incomes', methods=['GET', 'POST', 'PUT', 'DELETE'])
+@login_required
 def incomes():
     ...
 
 @main.route('/expenses', methods=['GET', 'POST', 'PUT', 'DELETE'])
+@login_required
 def expenses():
     ...
 
 @main.route('/budgets', methods=['GET', 'POST', 'PUT', 'DELETE'])
+@login_required
 def budgets():
     ...
 
 @main.route('/analyzes', methods=['GET'])
+@login_required
 def analyzes():
     ...
 
 @main.route('/profile', methods=['GET', 'PUT'])
+@login_required
 def profile():
     ...
 
 @main.route('/settings')
+@login_required
 def settings():
     ...
