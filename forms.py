5403ed551b529c9be8761c464f875ca3e14232ff
Vinícius Gonçalves <vgo1@aluno.ifal.edu.br>
2023-05-04 00:31:14 -0300

    feat: adiciona formulário esqueceu senha

--- forms.py
+++ forms.py
@@ -2,14 +2,18 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length
 
-class FormEntrar(FlaskForm):
+class LoginForm(FlaskForm):
     email = StringField(validatos=[InputRequired()])
     password = PasswordField(validators=[InputRequired()])
     submit = SubmitField('Entrar')
 
-class FormCadastrar(FlaskForm):
+class SignupForm(FlaskForm):
     name = StringField(validatos=[InputRequired()])
     email = StringField(validators=[InputRequired()])
     password = PasswordField(validators=[InputRequired()])
     password2 = PasswordField(validators=[InputRequired()])
     submit = SubmitField('Cadastrar-se')
+
+class RecoveryForm(FlaskForm):
+    email = StringField(validators=[InputRequired()])
+    submit = SubmitField('Enviar')
