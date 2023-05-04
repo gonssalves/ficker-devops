acb4efbc3e9dd223bdbf820d35a29510212d249f
Vinícius Gonçalves <vgo1@aluno.ifal.edu.br>
2023-05-04 00:16:27 -0300

    style: muda nome variáveis

--- forms.py
+++ forms.py
@@ -4,12 +4,12 @@ from wtforms.validators import InputRequired, Length
 
 class FormEntrar(FlaskForm):
     email = StringField(validatos=[InputRequired()])
-    senha = PasswordField(validators=[InputRequired()])
-    enviar = SubmitField('Entrar')
+    password = PasswordField(validators=[InputRequired()])
+    submit = SubmitField('Entrar')
 
 class FormCadastrar(FlaskForm):
-    nome = StringField(validatos=[InputRequired()])
+    name = StringField(validatos=[InputRequired()])
     email = StringField(validators=[InputRequired()])
-    senha = PasswordField(validators=[InputRequired()])
-    senha2 = PasswordField(validators=[InputRequired()])
-    enviar = SubmitField('Cadastrar-se')
+    password = PasswordField(validators=[InputRequired()])
+    password2 = PasswordField(validators=[InputRequired()])
+    submit = SubmitField('Cadastrar-se')
