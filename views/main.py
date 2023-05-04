e6ebb9f19c1932127c3f89e23112af423a52cc5a
Vinícius Gonçalves <vgo1@aluno.ifal.edu.br>
2023-05-04 00:15:43 -0300

    feat: adiciona views vazias

--- views/main.py
+++ views/main.py
@@ -6,6 +6,30 @@ main = Blueprint('main', __name__)
 def index():
     ...
 
-@main.route('/', methods=['GET'])
-def index():
+@main.route('/home', methods=['GET', 'POST', 'PUT'])
+def home():
+    ...
+
+@main.route('/incomes', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def incomes():
+    ...
+
+@main.route('/expenses', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def expenses():
+    ...
+
+@main.route('/budgets', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def budgets():
+    ...
+
+@main.route('/analyzes', methods=['GET'])
+def analyzes():
+    ...
+
+@main.route('/profile', methods=['GET', 'PUT'])
+def profile():
+    ...
+
+@main.route('/settings')
+def settings():
     ...
