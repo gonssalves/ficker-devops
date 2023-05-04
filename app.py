7bd9bd8e62c822265c2021fd4a731b65e1258cce
Vinícius Gonçalves <vgo1@aluno.ifal.edu.br>
2023-05-03 23:57:25 -0300

    feat: registra blueprints e inicializa LoginManager

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from views.main import main as view_main
 import os
 
 #caminho relativo
@@ -8,11 +10,23 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'ayofumylu'
+app.config['SECRET_KEY'] = 'hard to guess stringgggg'
 #configura o SQLite
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+
 #cria extensões
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    ''' Atualiza o objeto usuário com a ID do usuário armazenada na sessão. '''
+    from models.entities import Usuario
+    return Usuario.query.get(int(user_id))
+
+#registra o blueprint (blueprints lidam com as rotas)
+app.register_blueprint(view_main)
+app.register_blueprint(view_main)
