0e11048caba8bb1b5947982a417095c6007d8102
Vinícius Gonçalves <vgo1@aluno.ifal.edu.br>
2023-05-04 00:31:57 -0300

    feat: preenche views

--- views/auth.py
+++ views/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, url_for
+from flask import Blueprint, redirect, url_for, request, render_template
 from flask_login import login_required, logout_user
 
 auth = Blueprint('auth', __name__)
@@ -6,16 +6,34 @@ auth = Blueprint('auth', __name__)
 #views responsáveis pela autorização
 
 @auth.route('/login', methods=['GET', 'POST'])
-def entrar():
-    ...
+def login():
+    from forms import LoginForm
+    form = LoginForm()
+    if form.validate_on_submit():
+        from models.auth import auth_login
+        return auth_login()
+    form.process(request.args)
+    return render_template('', form=form)
 
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
-    ...
+    from forms import SignupForm
+    form = SignupForm()
+    if form.validate_on_submit():
+        from models.auth import auth_signup
+        return auth_signup()
+    form.process(request.args)
+    return render_template('', form=form)
 
-@auth.route('/forgot-password', methods=['GET', 'POST'])
-def forgot_password():
-    ...
+@auth.route('/recovery-password', methods=['GET', 'POST'])
+def recovery_password():
+    from forms import RecoveryForm
+    form = RecoveryForm()
+    if form.validate_on_submit():
+        from models.auth import auth_recovery
+        return auth_recovery()
+    form.process(request.args)
+    return render_template('', form=form)
 
 @auth.route('/logout', methods=['GET'])
 @login_required
