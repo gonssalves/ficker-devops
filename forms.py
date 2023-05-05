2b66e908a408553d3c774afbf9fa103f78677f01
Vinícius Gonçalves <103079267+gonssalves@users.noreply.github.com>
2023-05-05 19:56:48 -0300

    add: atualiza validators

--- forms.py
+++ forms.py
@@ -1,22 +1,32 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import InputRequired, Length
+from wtforms.validators import InputRequired, Length, Regexp
 
 class LoginForm(FlaskForm):
-    email = StringField(validatos=[InputRequired()])
-    password = PasswordField(validators=[InputRequired()])
+    email = StringField(validators=[InputRequired(message='Campo em branco')])
+    password = PasswordField('Senha', validators=[InputRequired(), Length(8, message='Senha precisa ter ao menos 8 caracteres')])
     remember_me = BooleanField('Me mantenha conectado')
     submit = SubmitField('Entrar')
 
 class SignupForm(FlaskForm):
-    real_name = StringField(validatos=[InputRequired(), Length(3)])
-    username = StringField(validatos=[InputRequired(), Length(3)])
-    email = StringField(validators=[InputRequired()])
-    password = PasswordField(validators=[InputRequired(), Length(8)])
-    password2 = PasswordField(validators=[InputRequired()])
+    real_name = StringField('Nome Real', validatos=[InputRequired(message='Campo em branco')])
+    username = StringField(
+        validators=[
+            InputRequired(), 
+            Length(3, message="Nome de usuário precisa ter ao menos 3 caracteres"), 
+            Regexp( 
+                "^[A-Za-z][A-Za-z0-9_.]*$", 
+                0, 
+                "Nomes de usuário só podem conter letras, " "números, pontos ou underline"
+            )
+        ]
+    )
+    email = StringField(validators=[InputRequired(message='Campo em branco')])
+    password = PasswordField('Senha', validators=[InputRequired(), Length(8, message='Senha precisa ter ao menos 8 caracteres')])
+    password2 = PasswordField('Repita a senha', validators=[InputRequired(message='Campo em branco')])
     submit = SubmitField('Cadastrar-se')
 
 class RecoveryForm(FlaskForm):
-    email = StringField(validators=[InputRequired()])
+    email = StringField(validators=[InputRequired(message='Campo em branco')])
     submit = SubmitField('Enviar')
 
