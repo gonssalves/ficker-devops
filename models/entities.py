6c191d974ae788cd408ab596bad97bc367127bf1
Vinícius Gonçalves <vgo1@aluno.ifal.edu.br>
2023-05-06 00:00:07 -0300

    feat: add método show_all

--- models/entities.py
+++ models/entities.py
@@ -45,6 +45,9 @@ class TransacaoEntrada(db.Model):
     def __repr__(self):
         return f'<Transação Entrada: {self.dsc_entrada} | Data: {self.dat_entrada} | Valor: {self.val_entrada}>'
     
+    def show_all():
+        return TransacaoEntrada.query.all()
+
 class TransacaoSaida(db.Model):
     __tablename__ = 'transacoes_saida'
     id = db.Column(db.Integer, primary_key=True)
