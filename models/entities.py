55bdaf5b81a4c487d81fc706dc708f851a3fc477
Vinícius Gonçalves <vgo1@aluno.ifal.edu.br>
2023-06-04 17:06:40 -0300

    feat: add métodos para fornecer valores a rota análises

--- models/entities.py
+++ models/entities.py
@@ -27,6 +27,108 @@ class Usuario(db.Model, UserMixin):
     def show_all():
         return Usuario.query.all()
     
+    def monthly(self):
+
+        income_jan = 0
+        income_feb = 0
+        income_mar = 0
+        income_apr = 0
+        income_may = 0
+        income_jun = 0
+        income_jul = 0
+        income_aug = 0
+        income_sep = 0
+        income_oct = 0
+        income_nov = 0
+        income_dec = 0
+        expense_jan = 0
+        expense_feb = 0
+        expense_mar = 0
+        expense_apr = 0
+        expense_may = 0
+        expense_jun = 0
+        expense_jul = 0
+        expense_aug = 0
+        expense_sep = 0
+        expense_oct = 0
+        expense_nov = 0
+        expense_dec = 0
+        
+        for transacao in self.transacoes_entrada_u: 
+            if transacao.dat_entrada.month == 1: income_jan += transacao.val_entrada
+            elif transacao.dat_entrada.month == 2: income_feb += transacao.val_entrada
+            elif transacao.dat_entrada.month == 3: income_mar += transacao.val_entrada
+            elif transacao.dat_entrada.month == 4: income_apr += transacao.val_entrada
+            elif transacao.dat_entrada.month == 5: income_may += transacao.val_entrada
+            elif transacao.dat_entrada.month == 6: income_jun += transacao.val_entrada
+            elif transacao.dat_entrada.month == 7: income_jul += transacao.val_entrada
+            elif transacao.dat_entrada.month == 8: income_aug += transacao.val_entrada
+            elif transacao.dat_entrada.month == 9: income_sep += transacao.val_entrada
+            elif transacao.dat_entrada.month == 10: income_oct += transacao.val_entrada
+            elif transacao.dat_entrada.month == 11: income_nov += transacao.val_entrada
+            elif transacao.dat_entrada.month == 12: income_dec += transacao.val_entrada
+        
+        for transacao in self.transacoes_saida_u: 
+            if transacao.dat_saida.month == 1: expense_jan += transacao.val_saida
+            elif transacao.dat_saida.month == 2: expense_feb += transacao.val_saida
+            elif transacao.dat_saida.month == 3: expense_mar += transacao.val_saida
+            elif transacao.dat_saida.month == 4: expense_apr += transacao.val_saida
+            elif transacao.dat_saida.month == 5: expense_may += transacao.val_saida
+            elif transacao.dat_saida.month == 6: expense_jun += transacao.val_saida
+            elif transacao.dat_saida.month == 7: expense_jul += transacao.val_saida
+            elif transacao.dat_saida.month == 8: expense_aug += transacao.val_saida
+            elif transacao.dat_saida.month == 9: expense_sep += transacao.val_saida
+            elif transacao.dat_saida.month == 10: expense_oct += transacao.val_saida
+            elif transacao.dat_saida.month == 11: expense_nov += transacao.val_saida
+            elif transacao.dat_saida.month == 12: expense_dec += transacao.val_saida
+            
+            dicio_incomes = {'jan': income_jan,"feb": income_feb, "mar": income_mar, "apr": income_apr, "may": income_may,"jun": income_jun,"jul": income_jul,"aug": income_aug,"sep": income_sep,"oct": income_oct,"nov": income_nov,"dec": income_dec}
+            dicio_expenses = {'jan': expense_jan,"feb": expense_feb, "mar": expense_mar, "apr": expense_apr, "may": expense_may,"jun": expense_jun,"jul": expense_jul,"aug": expense_aug,"sep": expense_sep,"oct": expense_oct,"nov": expense_nov,"dec": expense_dec}
+            
+            lista = [dicio_incomes, dicio_expenses]
+
+        return lista
+    
+    def total(self):
+        entrada = 0
+        saida = 0
+        cofrinho = 0
+
+        for transacao in self.transacoes_entrada_u: 
+            entrada += 1
+
+        for transacao in self.transacoes_saida_u: 
+            saida += 1
+        
+        for transacao in self.transacoes_cofrinho_u: 
+            cofrinho += 1
+
+        total = entrada + saida + cofrinho
+
+        dicio =	{ "entrada": entrada, "saida": saida, "cofrinho": cofrinho, "total": total }
+
+        return dicio
+    
+    def most_recents(self):
+
+        transactions = []
+
+        for transacao in self.transacoes_entrada_u: 
+            dicio = {'date': transacao.dat_entrada, 'value': transacao.val_entrada, 'type': 'Entrada'}
+            transactions.append(dicio)
+
+        for transacao in self.transacoes_saida_u:
+            dicio = {'date': transacao.dat_saida, 'value': transacao.val_saida, 'type': 'Saída'}
+            transactions.append(dicio)
+
+        for transacao in self.transacoes_cofrinho_u:
+            dicio = {'date': transacao.dat_transacao, 'value': transacao.val_cofrinho, 'type': f'Cofrinho - {transacao.tip_transacao}'}
+            transactions.append(dicio)
+
+        most_recents = sorted(transactions, key=lambda x: x['date'], reverse=True)[:5]
+
+        return most_recents
+
 class Orcamento(db.Model):
     __tablename__ = 'orcamentos'
     id = db.Column(db.Integer, primary_key=True)
@@ -110,7 +212,7 @@ class TransacaoCofrinho(db.Model):
     def show_one(budget_id):
         return TransacaoCofrinho.query.get(int(budget_id))
     
-    objetivos = db.relationship('Objetivo', backref='transacao_cofrinho', lazy=True)
+    #objetivos_c = db.relationship('Objetivo', backref='transacao_cofrinho', lazy=True)
 
         
     # def __repr__(self):
