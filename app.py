5f6848aa83b35893edfe3290353a2a3f81aee626
gonssalves <vgo1@aluno.ifal.edu.br>
2024-07-31 15:08:42 -0300

    12 factor

--- app.py
+++ app.py
@@ -9,19 +9,19 @@ from views.main import main as view_main
 from views.auth import auth as view_auth
 from dotenv import load_dotenv
 from sqlalchemy import text
-
+from celery import Celery
 import os, sentry_sdk
 
 # Função para conectar ao banco de dados usando um socket Unix
 def connect_unix_socket():
     """Inicializa um pool de conexão usando um socket Unix para uma instância do Cloud SQL do PostgreSQL."""
-    db_user = os.environ["DB_USER"]  # Substitua pelo nome de usuário do seu banco de dados
-    db_pass = os.environ["DB_PASS"]  # Substitua pela senha do seu banco de dados
-    db_name = os.environ["DB_NAME"]  # Substitua pelo nome do seu banco de dados
-    unix_socket_path = os.environ["INSTANCE_UNIX_SOCKET"]  # Substitua pelo caminho do soquete Unix da sua instância do Cloud SQL
+    db_user = os.environ["DB_USER"]
+    db_pass = os.environ["DB_PASS"]
+    db_name = os.environ["DB_NAME"]
+    unix_socket_path = os.environ["INSTANCE_UNIX_SOCKET"]
 
     pool = sqlalchemy.create_engine(
-            sqlalchemy.engine.url.URL.create(
+        sqlalchemy.engine.url.URL.create(
             drivername="postgresql+pg8000",
             username=db_user,
             password=db_pass,
@@ -45,29 +45,41 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False    
 
-    app.config['MAIL_SERVER']='smtp.gmail.com'
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
     app.config['MAIL_PORT'] = 587
     app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_SENDER')
     app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASSWORD')
     app.config['MAIL_TLS'] = True
     app.config['MAIL_SSL'] = False
 
-    # Configuração do sentry
+    # Configuração do Sentry
     sentry_sdk.init(
         dsn="https://307d0e84cf2281bab01212d9862c73b1@o4506650077822976.ingest.sentry.io/4506650093813760",
-        # Set traces_sample_rate to 1.0 to capture 100%
-        # of transactions for performance monitoring.
         traces_sample_rate=1.0,
-        # Set profiles_sample_rate to 1.0 to profile 100%
-        # of sampled transactions.
-        # We recommend adjusting this value in production.
         profiles_sample_rate=1.0,
     )
 
+    # Configuração do Celery
+    app.config['CELERY_BROKER_URL'] = 'redis://red-cql7qkaj1k6c73ceta20:6379'
+    app.config['CELERY_RESULT_BACKEND'] = 'redis://red-cql7qkaj1k6c73ceta20:6379'
+
     return app
+
+# Função para criar e configurar o Celery
+def make_celery(app):
+    celery = Celery(
+        app.import_name,
+        backend=app.config['CELERY_RESULT_BACKEND'],
+        broker=app.config['CELERY_BROKER_URL']
+    )
+    celery.conf.update(app.config)
+    return celery
 
 # Cria o aplicativo Flask
 app = create_app()
+
+# Cria e configura o Celery
+celery = make_celery(app)
 
 # Cria a instância do SQLAlchemy
 db = SQLAlchemy(app)
@@ -78,7 +90,7 @@ mail = Mail(app)
 
 from models.entities import *
 
-#define a view responsável por login e ajuda a previnir o roubo da sessão do usuário
+# Define a view responsável por login e ajuda a prevenir o roubo da sessão do usuário
 login_manager.login_view = 'auth.login'
 login_manager.session_protection = 'strong'
 
@@ -88,7 +100,7 @@ def load_user(user_id):
     from models.entities import Usuario
     return Usuario.query.get(int(user_id))
 
-#registra o blueprint (blueprints lidam com as rotas)
+# Registra os blueprints
 app.register_blueprint(view_main)
 app.register_blueprint(view_auth)
 
@@ -105,12 +117,12 @@ def migrate_db():
             return f'Erro ao aplicar migrações do banco de dados: {str(e)}', 500
     else:
         return 'Método não suportado', 405
-    
+
 if __name__ == '__main__':
     import os
     if os.getenv('FLASK_ENV') == 'development':
         app.run(debug=True, host='0.0.0.0', port=5000)
     else:
         # Executa o aplicativo Flask usando o Gunicorn para produção
-        os.system('gunicorn -b 0.0.0.0:8080 app:app')
+        os.system('gunicorn -w 4 -b 0.0.0.0:8080 app:app')
         db_pool = connect_unix_socket()
